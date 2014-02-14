3f9ee46a5ac239b4dc6c00f0da77f3b2edc8351c
CatalystDP <cld18874@126.com>
2014-02-14 12:55:41 +0800

    last commit

---
+++ jsTemplate/html/js/Template/model.js
@@ -0,0 +1,20 @@
+/**
+ * Created by dp on 14-2-11.
+ */
+define(function(require,exports,module){
+    var body,$=jQuery,$dm=dm;
+    module.exports={
+        exModel:null,
+        init:function(b){
+            body=b;
+            this.exModel=run();
+        }
+    };
+    function run(){
+        var modelCollection={};
+
+        //your code goes here
+
+        return modelCollection;
+    }
+});
