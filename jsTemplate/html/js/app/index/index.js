bf8f7a363cb4580086776a22c02de8f1238e3065
CatalystDP <cld18874@126.com>
2014-03-23 16:12:59 +0800

    last commit

--- jsTemplate/html/js/app/index/index.js
+++ jsTemplate/html/js/app/index/index.js
@@ -1,7 +1,7 @@
 define(function (require, exports, module) {
     require.async("jquery", function () {
         require.async("jqueryColor", function () {
-            require.async("js/core/dp_mz_debug.js", function () {
+            require.async("js/core/dp_mz_v2.js", function () {
                 jQuery(function ($) {
                     require.async("./controller.js");
                 });
---
+++ jsTemplate/html/js/test.js
@@ -0,0 +1,5 @@
+
+controller.addRouter({
+    2:function(){},
+    3:function(){}
+});
