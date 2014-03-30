58fcf0f81f041897b7defaf6ed9cb5cd37f015b4
CatalystDP <cld18874@126.com>
2014-03-30 17:13:42 +0800

    增加页脚修复

---
+++ jsTemplate/html/js/selfmodule/fixfooter.js
@@ -0,0 +1,6 @@
+var bodyHeight=$(body).height(),
+                   	htmlHeight=$(html).height();
+                    var footer=$(#footer);
+                    if(bodyHeighthtmlHeight)
+                    	footer.addClass('l-footer-bottom');
+/*这段代码是复制到需要的地方进行单页面页脚贴底边的，根据需要修改*/
