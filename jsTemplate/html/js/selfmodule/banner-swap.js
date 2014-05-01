dc9fa033dd000d334739f11c774a14c6ff4928f2
CatalystDP <cld18874@126.com>
2014-05-01 20:00:19 +0800

    修改banner-swap.js

--- jsTemplate/html/js/selfmodule/banner-swap.js
+++ jsTemplate/html/js/selfmodule/banner-swap.js
@@ -1,25 +1,33 @@
 /**
  * Created by dp on 14-4-5.
  */
-(function ($,f,root) {
+(function($, f, root) {
     var proto = BannerSwap.prototype;
+    var body = $('body');
 
     function BannerSwap(context, option) {
         this.currentDomObject = context;
         this.option = option;
         this.children = this.currentDomObject.children();
-        this.singleWidth = this.children[0].width;
+        this.singleWidth = $('body').width();
         this.marginTable = [];
-        this.max=this.children.length;
-        this.current=[0];
-        this.interVal=root.undefined;
-        if(option.prev&&option.next)
-        {
-            this.prevBtn=option.prev;
-            this.nextBtn=option.next;
+        this.max = this.children.length;
+        this.current = [0];
+        this.interVal = root.undefined;
+        if (option.prev && option.next) {
+            this.prevBtn = option.prev;
+            this.nextBtn = option.next;
         }
-    }//创建bannerswap类
-    proto.calculateWidth = function () {
+    } //创建bannerswap类
+    proto.reCalculateWidth = function() {
+        // this.currentDomObject.css("margin-left", "0");
+        this.children.css("width", body.width());
+        this.singleWidth = $(this.children[0]).width();
+        this.marginTable.splice(0);
+        this.calculateWidth();
+        this.autoSwap();
+    };
+    proto.calculateWidth = function() {
         var length = this.children.length;
         var width = parseInt(this.singleWidth);
         var margin = 0;
@@ -30,71 +38,82 @@
             marginTable.push(margin + "px");
             margin -= width;
         }
-        this.canClick=true;
+        this.canClick = true;
     };
-    proto.slide = function (current,callback) {
+    proto.slide = function(current, callback) {
         var that = this;
-        that.canClick=false;
+        that.canClick = false;
         this.currentDomObject.stop().animate({
             marginLeft: that.marginTable[current]
-        }, this.option.duration,function(){
-            that.canClick=true;
-            if(callback)
+        }, this.option.duration, function() {
+            that.canClick = true;
+            if (callback)
                 callback();
         });
 
     };
-    proto.autoSwap = function () {
-        var that=this;
-        var current=this.current;
+    proto.autoSwap = function() {
+        var that = this;
+        var current = this.current;
 
-        this.interVal = setInterval(function () {
+        this.interVal = setInterval(function() {
             current[0] += 1;
             if (current[0] >= that.max)
                 current[0] = 0;
             that.slide(current[0]);
         }, this.option.delay);
     };
-    proto.navBtn=function(){
-        var current=this.current;
-        var that=this;
-        this.prevBtn.on("click",function(e){
+    proto.navBtn = function() {
+        var current = this.current;
+        var that = this;
+        this.prevBtn.on("click", function(e) {
             clearInterval(that.interVal);
-            current[0]-=1;
-            if(current[0]<0)
-                current[0]=that.max-1;
-            that.slide(current[0],function(){
+            current[0] -= 1;
+            if (current[0] < 0)
+                current[0] = that.max - 1;
+            that.slide(current[0], function() {
                 that.autoSwap();
-            })
+            });
         });
-        this.nextBtn.on("click",function(e){
+        this.nextBtn.on("click", function(e) {
             clearInterval(that.interVal);
-            current[0]+=1;
-            if(current[0]>=that.max)
-                current[0]=0;
-            that.slide(current[0],function(){
+            current[0] += 1;
+            if (current[0] >= that.max)
+                current[0] = 0;
+            that.slide(current[0], function() {
                 that.autoSwap();
-            })
+            });
         });
     };
-    proto.startSwapPlugin = function () {
+    proto.startSwapPlugin = function() {
         this.calculateWidth();
-        if(this.prevBtn&&this.nextBtn)
+        if (this.prevBtn && this.nextBtn)
             this.navBtn();
         this.autoSwap();
     };
-    $.fn.bannerSwap = function (option) {
+    $.fn.bannerSwap = function(option) {
         var defaultOption = {
-            delay: 3000,//每3秒换一次图片
-            duration: 1000//动画时间为1秒
+            delay: 3000, //每3秒换一次图片
+            duration: 1000 //动画时间为1秒
         };
         var op;
+
         if (!option)
             op = defaultOption;
         else
             op = $.extend(defaultOption, option);
         if (op.duration >= 3000)
             op = defaultOption;
-        new BannerSwap(this, op).startSwapPlugin();
+        if (this.children().length <= 1)
+            return;
+        this.children().css("width", body.width() + "px");
+        var s = new BannerSwap(this, op);
+        s.startSwapPlugin();
+        $(window).on("resize", function() {
+            var interval = s.interVal;
+            if (interval)
+                window.clearInterval(interval);
+            s.reCalculateWidth();
+        });
     };
-})(jQuery,false,window);
+})(jQuery, false, window);
