4ce76cf9b5da577e2bdda5e855e2b32d1ccd17a6
CatalystDP <cld18874@126.com>
2014-05-27 20:50:03 +0800

    修改fixfooter

--- jsTemplate/html/js/selfmodule/fixfooter.js
+++ jsTemplate/html/js/selfmodule/fixfooter.js
@@ -2,6 +2,6 @@
                     e=document.documentElement||document.body,
                    	allHeight=e.clientHeight;
                     var footer=$(".js-footer-wp");
-                    if(containerHeight<allHeight)
+                    if(containerHeight<=allHeight)
                     	footer.css({position:'absolute',bottom:'0px',zIndex:1});
 /*这段代码是复制到需要的地方进行单页面页脚贴底边的，根据需要修改*/
