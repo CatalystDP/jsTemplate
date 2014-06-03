ed406b898fba0171788edf4586111e9839ca8ff5
CatalystDP <cld18874@126.com>
2014-06-03 17:50:42 +0800

    更新页脚

--- jsTemplate/html/js/selfmodule/fixfooter.js
+++ jsTemplate/html/js/selfmodule/fixfooter.js
@@ -1,7 +1,12 @@
- var containerHeight=$("body")[0].scrollHeight,
-                    e=document.documentElement||document.body,
-                   	allHeight=e.clientHeight;
-                    var footer=$(".js-footer-wp");
-                    if(containerHeight<=allHeight)
-                    	footer.css({position:'absolute',bottom:'0px',zIndex:1});
-/*这段代码是复制到需要的地方进行单页面页脚贴底边的，根据需要修改*/
+fixFooter();
+$(window).resize(function(){
+    fixFooter();
+});
+function fixFooter(){
+    var containerHeight=$("body")[0].scrollHeight,
+        e=document.documentElement||document.body,
+        allHeight=e.clientHeight;
+    var footer=$(".js-footer-wp");
+    if(containerHeight<=allHeight)
+        footer.css({position:'absolute',bottom:'0px',zIndex:1});
+}
